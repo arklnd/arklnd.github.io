51f4cb39e9a7125a4d9667ffba625a32a50d3d22
Arijit Kundu <Arijit.Kundu@hyland.com>
2026-04-15 08:51:51 +0530

    feat: update markdown theme configurations to use one-light and one-dark-pro

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ export default defineConfig({
   markdown: {
     shikiConfig: {
       themes: {
-        light: "github-light",
-        dark: "github-dark",
+        light: "one-light",
+        dark: "one-dark-pro",
       },
       defaultColor: false,
     },
