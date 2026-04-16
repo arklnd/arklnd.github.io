02bbab4a286d2ed5ac7a99031034687c930bb98f
Arijit Kundu <Arijit.Kundu@hyland.com>
2026-04-16 21:32:05 +0530

    feat: add sitemap integration for improved SEO and site navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig, envField } from "astro/config";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import { remarkMermaid } from "./src/utils/remarkMermaid";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
 export default defineConfig({
   site: "https://arklnd.github.io",
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   env: {
     schema: {
       HITS_KEY: envField.string({ context: "server", access: "public", default: "dev" }),
